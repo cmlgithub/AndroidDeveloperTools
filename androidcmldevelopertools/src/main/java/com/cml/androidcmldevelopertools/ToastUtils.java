db9dd38ab1451a3080ea017eb67752e55616ba69
chenmingliang <229473390@qq.com>
2016-11-30 14:26:24 +0800

    first commit

---
+++ androidcmldevelopertools/src/main/java/com/cml/androidcmldevelopertools/ToastUtils.java
@@ -0,0 +1,19 @@
+package com.cml.androidcmldevelopertools;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+public class ToastUtils {
+
+    public static void toastShort(Context context,String toast){
+        if(context != null && !TextUtils.isEmpty(toast)){
+            Toast.makeText(context,toast,Toast.LENGTH_SHORT).show();
+        }
+    }
+    public static void toastLong(Context context,String toast){
+        if(context != null && !TextUtils.isEmpty(toast)){
+            Toast.makeText(context,toast,Toast.LENGTH_LONG).show();
+        }
+    }
+}
